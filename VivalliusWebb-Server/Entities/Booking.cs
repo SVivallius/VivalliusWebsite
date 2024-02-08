3c65480593924724961bc45cc567029d07bef35c
Stefan Vivallius <varulvsnatt@hotmail.com>
2024-02-08 08:44:28 +0100

    Corrected entity booking to represent its appearance on the already existing frontend.

--- VivalliusWebb-Server/Entities/Booking.cs
+++ VivalliusWebb-Server/Entities/Booking.cs
@@ -3,10 +3,9 @@ namespace VivalliusWebb_Server.Entities;
 public class Booking : IEntity
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Message { get; set; }
+    public string Name { get; set; }
     public string Email { get; set; }
-    public string Phone { get; set; }
+    public string Genre { get; set; }
+    public string Message { get; set; }
     public bool IsComplete { get; set; } = false;
 }
