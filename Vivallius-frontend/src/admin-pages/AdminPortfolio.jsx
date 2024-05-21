98889b89f0232b495565ebb68e84bdf6457a0566
Stefan Vivallius <stefan.vivallius@outlook.com>
2024-05-21 16:35:24 +0200

    Begun work on portfolio page of admin layer.

--- Vivallius-frontend/src/admin-pages/AdminPortfolio.jsx
+++ Vivallius-frontend/src/admin-pages/AdminPortfolio.jsx
@@ -1,8 +1,81 @@
 import './styles/AdminPortfolio.css'
+import { HttpRequest } from '../assets/HttpAgent.js'
+import { useContext, useEffect, useState } from 'react'
+import AdminContext from '../assets/AdminContext.jsx'
 
 function AdminPortfolio() {
+    const {adminArgs} = useContext(AdminContext)
+    const [imgArray, setImgArray] = useState([])
+    const [formData, setFormData] = useState({
+        
+    })
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        let getImgs = () =>{
+            let args = {
+                method: 'GET',
+                headers: {
+                    'content-type': 'application/json'
+                }
+            }
+            HttpRequest('1/public/photo', args)
+                .then((response) => {
+                    if (response.ok){
+                        return response.json()
+                    }
+                    else throw new Error
+                })
+                .then((json) => {
+                    let t = []
+                    json.map((item) => {
+                        t.push(item)
+                    })
+                    setImgArray(t)
+                    setIsLoading(false)
+                })
+                .catch((error) => {
+                    setImgArray([])
+                    setIsLoading(false)
+                })
+        }
+        getImgs()
+    }, [])
+
+    const handleFormInputChange = (event) => {
+        const {name, value} = event.target
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (args) => {
+        args.preventDefault()
+        let rqstArgs = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-Vivallius-Admin-Token': adminArgs.context.token
+            },
+            body: JSON.stringify(formData)
+        }
+        HttpRequest('1/admin/photo', rqstArgs)
+            .then((response) => {
+                if (response.ok){
+                    alert("Den nya bilden har lagts upp!")
+                } else throw new Error("Bilden gick inte att ladda upp.")
+            })
+            .catch((error) => {
+                alert(error.message)
+            })
+    }
     return (
         <>
+            <div className={`admin-folio-add-img`}>
+
+            </div>
         </>
     )
 }
